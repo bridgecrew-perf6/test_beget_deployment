c91a8e14b407599e0d3086b98fddc52fe3b47607
Moraiene <vladislav.ponamaryov@gmail.com>
2022-06-17 19:12:28 +0300

    init

--- scripts/beget_deploy.sh
+++ scripts/beget_deploy.sh
@@ -1,7 +1,6 @@
 git reset --hard
 git clean -df
 git pull origin prod
-composer-php8.1 install --no-interaction --prefer-dist --optimize-autoloader --no-dev --ignore-platform-reqs
 ssh localhost -p 222
 cd test
 cd npm install
